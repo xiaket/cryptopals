c42ba320102d1997f780a55089405a90df3674b6
Kai Xia <xiaket@gmail.com>
2018-04-24 06:55:02 +1000

    create lib package and use it.
    Signed-off-by: Kai Xia <xiaket@gmail.com>

---
+++ lib/cryptopals.go
@@ -0,0 +1,53 @@
+package cryptopals
+
+import "strings"
+
+var frequency = map[byte]float64{
+	'a': 0.08167,
+	'b': 0.01492,
+	'e': 0.12702,
+	'c': 0.02782,
+	'd': 0.04253,
+	'f': 0.02228,
+	'g': 0.02015,
+	'h': 0.06094,
+	'i': 0.06966,
+	'j': 0.00153,
+	'k': 0.00772,
+	'l': 0.04025,
+	'm': 0.02406,
+	'n': 0.06749,
+	'o': 0.07507,
+	'p': 0.01929,
+	'q': 0.00095,
+	'r': 0.05987,
+	's': 0.06327,
+	't': 0.09056,
+	'u': 0.02758,
+	'v': 0.00978,
+	'w': 0.02360,
+	'x': 0.00150,
+	'y': 0.01974,
+	'z': 0.00074,
+}
+
+func CalcRating(msg string) float64 {
+	rating := 0.0
+	for _, ch := range msg {
+		if ch < 32 && ch > 0 {
+			rating -= 0.1
+		} else {
+			lowered_character := []byte(strings.ToLower(string(ch)))[0]
+			rating += frequency[lowered_character]
+		}
+	}
+	return rating
+}
+
+func SafeXORBytes(dst []byte, a []byte, b byte) int {
+	n := len(a)
+	for i := 0; i < n; i++ {
+		dst[i] = a[i] ^ b
+	}
+	return n
+}
